e10220c1c48d9c8082c9eb5d6a2308b0fdb43b77
Ysom <1083688285@qq.com>
2019-11-21 15:58:04 +0800

    增加工具函数说明注释

--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,15 @@
-const Compare = {
+export const Compare = {
   LESS_THAN: -1,
   BIGGER_THAN: 1
 }
 
+
+// 判断值是否相等
 export function defaultEquals(a, b) {
   return a === b
 }
 
+// 比较两值大小
 export function defaultCompare(a, b) {
   if (a === b) {
     return 0
